019efe9c7538a57cef5288bfc4a85db55ea549d3
akram izzi <akramezzy5@gmail.com>
2025-12-14 19:19:17 +1000

    شرح التعديلات التي قمت بها

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49f64da26a5c98ae20b54dacd090676a56d9eb5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d795f97d0baa4afb41be5dd22b1618ea9166306")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
