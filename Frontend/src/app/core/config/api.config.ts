be046075fb0d3e708979c449067d65f301361453
akram izzi <akramezzy5@gmail.com>
2025-12-25 15:48:49 +1000

    S

--- Frontend/src/app/core/config/api.config.ts
+++ Frontend/src/app/core/config/api.config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL: string =
-  (window as { __API_BASE_URL__?: string }).__API_BASE_URL__ || 'http://localhost:7150';
+  (window as { __API_BASE_URL__?: string }).__API_BASE_URL__ || 'https://localhost:7150';
