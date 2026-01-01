7816ba04c579ae150cd37ece62153726161fe488
akram izzi <akramezzy5@gmail.com>
2026-01-02 02:40:10 +1000

    F

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a8ec92450cf6281a6fca18b2f5ae9279f2c473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee2a92d24f821942b1a7bef9db2b5390eb1cf62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
